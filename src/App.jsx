abefd72ab4ad08789de1fbbb28341685375736cb
bhushanpatil288 <sharewithbhushan@gmail.com>
2026-01-20 14:40:58 +0530

    [add] - particles background

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import ParticlesComponent from './components/ParticlesComponent'
+
+function App() {
+  return (
+    <div>
+      <ParticlesComponent id="particles" />
+      Test
+    </div>
+  )
+}
+
+export default App
