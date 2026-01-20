3b0ff5d90e39acc298327eb505a3a48332c9270f
bhushanpatil288 <sharewithbhushan@gmail.com>
2026-01-20 15:35:34 +0530

    [add] - simple navbar

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,11 +1,22 @@
+import "../../../public/css/header/navbar.css"
+import { navLinks } from "../../constants/constants"
+
 const Navbar = () => {
   return (
-    <nav>
+    <nav className="navbar px-5 py-5 flex align-center justify-between">
       {/* logo */}
-      
-      {/* nav items */}
+      <div className="flex justify-center items-center">
+        <p className="glitch">Bhushan's Portfolio</p>
+      </div>
 
-      {/* others */}
+      {/* nav items */}
+      <ul className="flex gap-3">
+        {navLinks.map((link, idx)=>{
+            return <li key={idx} className="flex justify-center items-center">
+                        <a className="nav-links" href={link.url} target={link.target} title={link.title}>{link.name.charAt(0).toUpperCase() + link.name.slice(1)}</a>
+                    </li>
+        })}
+      </ul>
     </nav>
   )
 }
---
+++ src/constants/constants.js
@@ -0,0 +1,30 @@
+// Header start
+
+// Navbar start
+ 
+export const navLinks = [
+    {
+        name: "Home",
+        url: "#",
+        target: "_self",
+        title: "Visit Home page"
+    },
+    {
+        name: "About",
+        url: "#",
+        target: "_blank",
+        title: "Visit About page"
+    },
+    {
+        name: "Contact",
+        url: "#",
+        target: "_blank",
+        title: "Visit Contact page"
+    }
+]
+
+
+
+// Navbar end
+
+// Header end
